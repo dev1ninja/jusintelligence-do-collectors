b031424336d70392b67425dc5a94cae76bfc2987
Gianfranco <devninja.up@gmail.com>
2021-10-26 06:30:44 -0500

    not completed

---
+++ do-trf3-collector-service/src/index.js
@@ -0,0 +1,28 @@
+const springCloudConfigClient = require("cloud-config-client");
+const express = require("express");
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+const port = 3000;
+let microserviceName = "do-processor-trf3";
+let ambiente = process.env.AMBIENTE || "local";
+
+springCloudConfigClient.load({
+    endpoint: 'https://scc-dev.dataseed.de:443',
+    name: microserviceName,
+    auth: { user: "root", pass: "s3cr3t"},
+    profiles: [ambiente] })
+  .then(config => {
+    require("./routes")(app, config);
+    // const consume = require("./config/kafka-consumer")(config, require("./listener/listener-search-url"), ambiente);
+    // consume().catch((err) => {
+    //   console.error("error in consumer: ", err);
+    // })
+    const consume = async () => require("./listener/listener-search-url")(config, '{"search": "something", "date_ini": "2021-10-16", "date_end": "2021-10-18"}', ambiente)
+    consume();
+    // app.listen(port, () =>
+    //   console.log(`App working at http://localhost:${port}`)
+    // );
+
+  }).catch(console.error)
---
+++ do-trf3-collector-service/src/reqParams/urls.js
@@ -0,0 +1,7 @@
+const HOME_PAGE_URL = 'https://www.trf3.jus.br/';
+const ORIGIN_PAGE_URL = 'http://web.trf3.jus.br/diario/Consulta';
+const REFERER_URL = 'http://web.trf3.jus.br/diario';
+
+module.exports.HOME_PAGE_URL = HOME_PAGE_URL;
+module.exports.ORIGIN_PAGE_URL = ORIGIN_PAGE_URL;
+module.exports.REFERER_URL = REFERER_URL;

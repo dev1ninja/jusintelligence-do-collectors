c5f826a2dec0309b800721467df2a0d87366c67f
Gianfranco <devninja.up@gmail.com>
2021-10-27 12:44:22 -0500

    final

---
+++ do-trf4-collector-service/src/downloader/main.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+const getAllDateList = require('./getAllfDateList');
+const getDownloadDateList = require('./getDownloadDateList');
+const getPdfListPerMonth = require('./getPdfListPerMonth');
+const downloadPdf = require('./download-pdf');
+const upload2aws = require('../s3bucket/upload');
+
+async function main( message, dest_dir, callback ) {
+
+  const allDateList = await getAllDateList();
+  // console.log(allDateList);
+
+  const downloadDateList = getDownloadDateList(message);
+  console.log(downloadDateList);
+  
+  const promiseList = [];
+  const allPdfList = [];
+
+  for(let i = 0; i < downloadDateList.length; i++){
+    if(allDateList.includes(downloadDateList[i])){
+      promiseList.push(await getPdfListPerMonth(downloadDateList[i], allPdfList, message));
+    }
+  }
+
+  await Promise.all(promiseList);
+
+  const downloads = [];
+  console.log(allPdfList.length)
+  for(let i = 0; i < allPdfList.length; i++){
+    downloads.push(downloadPdf(allPdfList[i].value, dest_dir));
+  }
+
+  console.log("------- PDF downloading started -------\n", downloads.length);
+  await Promise.all(downloads).then(value => {
+    console.log(value);
+    console.log("------- PDF downloading finished -------\n");
+    callback();
+  })
+}
+
+async function index( config, message, ambiente ) {
+  
+  const dest_dir = `./${message.date_ini}-${message.date_end}`; 
+  
+  //Create Director based on message's date
+  if(!fs.existsSync(dest_dir)){ 
+    fs.mkdirSync(dest_dir);
+  }
+
+  await main( message, dest_dir, async () => {
+
+    const sendJsonData = await upload2aws(dest_dir); // Upload all downloaded PDF files to AWS
+
+    for(let i = 0; i < sendJsonData.length; i++){
+      sendJsonData[i]['uf'] = 'TRF4';
+      sendJsonData[i]['search'] = message.search;
+    } // Finished to upload.
+
+    console.log("-----------------------");
+    const producer = require('../config/kafka-producer')(ambiente, sendJsonData); // Start to send message to `do_processor_final_<env>` kafka topic
+
+    producer().catch( err => {
+      console.error("erro in producer: ", err);
+    });
+  });
+  
+}
+
+module.exports = index;
---
+++ do-trf4-collector-service/src/reqParams/urls.js
@@ -0,0 +1,9 @@
+const SEARCH_QUERY_PAGE = 'https://www.trf4.jus.br/trf4/diario/consulta_diario.php';
+const SEARCH_PAGE_URL = 'https://www.trf4.jus.br/trf4/diario/edicoes_anteriores.php';
+const ORIGIN_PAGE_URL = 'https://www.trf4.jus.br';
+const DOWNLOAD_PREFIX = 'https://www.trf4.jus.br/trf4/diario/';
+
+module.exports.SEARCH_QUERY_PAGE = SEARCH_QUERY_PAGE;
+module.exports.SEARCH_PAGE_URL = SEARCH_PAGE_URL;
+module.exports.ORIGIN_PAGE_URL = ORIGIN_PAGE_URL;
+module.exports.DOWNLOAD_PREFIX = DOWNLOAD_PREFIX;

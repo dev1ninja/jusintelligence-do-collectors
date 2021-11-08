d3f8da087905b6fd27ed24e8186a1803fbb5c49a
Gianfranco <devninja.up@gmail.com>
2021-11-08 11:07:23 -0500

    Refactoring

--- do-trf4-collector-service/src/controller/model-controller.js
+++ do-trf4-collector-service/src/controller/model-controller.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (config) => {
-    router
-        .route("/save").post((req, res) => {
-            res.send("POST METHOD")
-        });
-    return router;
+  router
+    .route("/save").post((req, res) => {
+      res.send("POST METHOD")
+    });
+  return router;
 }

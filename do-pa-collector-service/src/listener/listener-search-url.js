857d1896ffa95c38e1e34da94f88253894ea9b4b
Gianfranco <devninja.up@gmail.com>
2021-11-17 10:59:42 -0500

    update

--- do-pa-collector-service/src/listener/listener-search-url.js
+++ do-pa-collector-service/src/listener/listener-search-url.js
@@ -4,7 +4,22 @@ module.exports = async (config, message, ambiente) => {
 
     let newMessage = JSON.parse(message);
 
-    console.log("Call downloader ----------------------------");
-    
-    await require("../downloader/downloader")(config, SEARCH_PAGE_URL, newMessage, ambiente);
+    if(newMessage.jusbrasil && newMessage.search != null){
+        const sendJsonData = {
+            "id": newMessage.id,
+            "search": newMessage.search,
+            "uf": "PA",
+            "jusbrasil": newMessage.jusbrasil,
+            "date_ini": newMessage.date_ini,
+            "date_end": newMessage.date_end
+        }
+        const producer = require('../config/kafka-producer')(ambiente, sendJsonData);
+        producer().catch( err => {
+            console.error("error in producer: ", err);
+        })
+    } else {
+        console.log("Call downloader ----------------------------");
+        await require("../downloader/downloader")(config, SEARCH_PAGE_URL, newMessage, ambiente);
+    }
+
 }

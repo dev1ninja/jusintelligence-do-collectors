312d940a7901f1c18944b74d166d173a3f47a3ca
Gianfranco <devninja.up@gmail.com>
2021-11-17 11:20:25 -0500

    final

--- do-trf1-collector-service/src/listener/listener-search-url.js
+++ do-trf1-collector-service/src/listener/listener-search-url.js
@@ -1,6 +1,21 @@
 module.exports = async (config, message, ambiente) => {
 
     let newMessage = JSON.parse(message);
+    if(newMessage.jusbrasil && newMessage.search != null){
+        const sendJsonData = {
+            "id": newMessage.id,
+            "search": newMessage.search,
+            "uf": "TRF1",
+            "jusbrasil": newMessage.jusbrasil,
+            "date_ini": newMessage.date_ini,
+            "date_end": newMessage.date_end
+        }
+        const producer = require('../config/kafka-producer')(ambiente, sendJsonData);
+        producer().catch( err => {
+            console.error("error in producer: ", err);
+        })
+    } else {
+        await require("../downloader/main")(config, newMessage, ambiente);
+    }
 
-    await require("../downloader/main")(config, newMessage, ambiente);
 }
--- do-trf3-collector-service/src/listener/listener-search-url.js
+++ do-trf3-collector-service/src/listener/listener-search-url.js
@@ -1,12 +1,27 @@
 module.exports = async (config, message, ambiente) => {
 
     let newMessage = JSON.parse(message);
-    let date_ini = new Date(newMessage.date_ini);
-    let date_end = new Date(newMessage.date_end);
-
-    if(date_ini > date_end){
-        throw new Error('Initial date should be less than end date.');
+    if(newMessage.jusbrasil && newMessage.search != null){
+        const sendJsonData = {
+            "id": newMessage.id,
+            "search": newMessage.search,
+            "uf": "TRF3",
+            "jusbrasil": newMessage.jusbrasil,
+            "date_ini": newMessage.date_ini,
+            "date_end": newMessage.date_end
+        }
+        const producer = require('../config/kafka-producer')(ambiente, sendJsonData);
+        producer().catch( err => {
+            console.error("error in producer: ", err);
+        })
+    } else {
+        let date_ini = new Date(newMessage.date_ini);
+        let date_end = new Date(newMessage.date_end);
+    
+        if(date_ini > date_end){
+            throw new Error('Initial date should be less than end date.');
+        }
+    
+        await require("../downloader/main")(config, newMessage, ambiente);
     }
-
-    await require("../downloader/main")(config, newMessage, ambiente);
 }

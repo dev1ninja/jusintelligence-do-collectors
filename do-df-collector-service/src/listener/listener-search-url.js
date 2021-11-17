3204370e89a375aab60ec6990a05b8dac87858dd
Gianfranco <devninja.up@gmail.com>
2021-11-17 10:49:24 -0500

    update jusbrasil

--- do-df-collector-service/src/listener/listener-search-url.js
+++ do-df-collector-service/src/listener/listener-search-url.js
@@ -2,5 +2,20 @@ module.exports = async (config, message, ambiente) => {
 
     let newMessage = JSON.parse(message);
 
+    if(newMessage.jusbrasil && newMessage.search != null){
+        const sendJsonData = {
+            "id": newMessage.id,
+            "search": newMessage.search,
+            "uf": "DF",
+            "jusbrasil": newMessage.jusbrasil,
+            "date_ini": newMessage.date_ini,
+            "date_end": newMessage.date_end
+        }
+        const producer = require('../config/kafka-producer')(ambiente, sendJsonData);
+        producer().catch( err => {
+            console.error("error in producer: ", err);
+        })
+    }
+
     await require("../downloader/main")(config, newMessage, ambiente);
 }

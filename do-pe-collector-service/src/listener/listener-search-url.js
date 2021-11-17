301a43f828051c9fa5c0d6131b451a10ef7e429f
Gianfranco <devninja.up@gmail.com>
2021-11-17 11:01:39 -0500

    add jusbrasil

--- do-pe-collector-service/src/listener/listener-search-url.js
+++ do-pe-collector-service/src/listener/listener-search-url.js
@@ -1,6 +1,20 @@
 module.exports = async (config, message, ambiente) => {
 
     let newMessage = JSON.parse(message);
-
-    await require("../downloader/main")(config, newMessage, ambiente);
+    if(newMessage.jusbrasil && newMessage.search != null){
+        const sendJsonData = {
+            "id": newMessage.id,
+            "search": newMessage.search,
+            "uf": "PE",
+            "jusbrasil": newMessage.jusbrasil,
+            "date_ini": newMessage.date_ini,
+            "date_end": newMessage.date_end
+        }
+        const producer = require('../config/kafka-producer')(ambiente, sendJsonData);
+        producer().catch( err => {
+            console.error("error in producer: ", err);
+        })
+    } else {
+        await require("../downloader/main")(config, newMessage, ambiente);
+    }
 }
